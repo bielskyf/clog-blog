64a2225388b5fd8b51d29701cfef13c417b257e9
bielskyf <gabriel.fernandesnogueira2008@gmail.com>
2026-02-06 22:06:40 -0300

    Adicionado controllers para as principais classes

---
+++ clog-api/clog-api/Controllers/NichoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace clog_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NichoController : ControllerBase
+    {
+    }
+}
--- clog-api/clog-api/obj/Debug/net9.0/clog-api.AssemblyInfo.cs
+++ clog-api/clog-api/obj/Debug/net9.0/clog-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clog-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec5f9b1b9894989a14fc40dc13afa8b4affd47b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ca1ec64e89f97ee5ad23b06dceaad2fc6d2cd83")]
 [assembly: System.Reflection.AssemblyProductAttribute("clog-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clog-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
